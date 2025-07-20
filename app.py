8ce173deff5af69d1dea78b3e0fbfec1c2768000
JoKa <143833423+jokalzz@users.noreply.github.com>
2025-07-21 01:20:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -365,4 +365,4 @@ def download_excel():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
